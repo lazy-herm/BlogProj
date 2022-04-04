3c4d9cca4bd9aa7adc17e6131b59fda657616f65
Marc Vautheny <marc@vautheny.com>
2022-04-04 09:46:48 +0100

    added post and comment model and changed settings lagnuage and timezone to gb

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.utils import timezone
+from django.urls import reverse
 # Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publich(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+
+    def __str__(self):
+        return self.title
+
+class Comments(models.Model):
+    post = models.ForeignKey('BlogApp.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateField(default = timezone.now())
+    approved_comment = models.BooleanField(default=False)
