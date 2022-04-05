5d6acdcaef581949153d7ffd1848be096ab30512
Marc Vautheny <marc@vautheny.com>
2022-04-05 10:32:07 +0100

    added approve and str method to commets mdoel

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
         self.save()
 
     def approve_comments(self):
-        return self.comments.filter(approved_comments=True)
+        return self.comments.filter(approved_comment=True)
 
     def __str__(self):
         return self.title
@@ -27,3 +27,10 @@ class Comments(models.Model):
     text = models.TextField()
     create_date = models.DateField(default = timezone.now())
     approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
