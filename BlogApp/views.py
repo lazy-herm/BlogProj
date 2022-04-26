8c827ef9d6519bd86857ad3343f11b9ae55b9c95
Marc Vautheny <marc@vautheny.com>
2022-04-26 09:44:03 +0100

    testing -f commit tag using a commit txt file.
    this is the second line of the commit.
    
    this is the third line of the commit.

--- BlogApp/views.py
+++ BlogApp/views.py
@@ -1,14 +1,16 @@
+from os import lseek
 from django.shortcuts import render
 from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView)
 from BlogApp.models import Post, Comment
 from BlogApp.forms import PostForm, CommentForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from datetime import timezone
 # Create your views here.
 
 
 class AboutView(TemplateView):
     template_name = 'about.html'
-
+    ##test for commit file
 class PostListView(ListView):
     model = Post
 
@@ -32,4 +34,8 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
     form_class = PostForm
     model = Post
 
+class DraftPostListView(ListView):
+    model = Post
 
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).orber_by('-published_date'))
