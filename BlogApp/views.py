1024202dc8d42eb2c61dcf4fe95c797159b288c0
Marc Vautheny <marc@vautheny.com>
2022-04-28 21:18:11 +0100

    added DeleteView and DraftListView
    - added delete post view class
    - added delete view url
    - added DraftListView view class
    - corrected typo on order_by methods in view classes
    - added DraftListView url path

--- BlogApp/views.py
+++ BlogApp/views.py
@@ -1,10 +1,11 @@
 from os import lseek
-from django.shortcuts import render
-from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView)
+from django.shortcuts import redirect, render
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
 from BlogApp.models import Post, Comment
 from BlogApp.forms import PostForm, CommentForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from datetime import timezone
+from django.urls import reverse_lazy
 # Create your views here.
 
 
@@ -15,7 +16,7 @@ class PostListView(ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now()).orber_by('-published_date'))
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
 class PostDetailView(DetailView):
     model = Post
@@ -38,4 +39,17 @@ class DraftPostListView(ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now()).orber_by('-published_date'))
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
