26f8c47014320734f9e364cd0ec694d213a275cb
Marc Vautheny <marc@vautheny.com>
2022-04-30 00:36:55 +0100

    Added blog file prefix to template names

--- BlogApp/views.py
+++ BlogApp/views.py
@@ -10,7 +10,7 @@ from django.urls import reverse_lazy
 
 
 class AboutView(TemplateView):
-    template_name = 'about.html'
+    template_name = 'blog/about.html'
     ##test for git alias
 class PostListView(ListView):
     model = Post
@@ -44,7 +44,7 @@ class DraftPostListView(ListView):
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
 
-    success_url = reverse_lazy('post_list')
+    success_url = reverse_lazy('blog/post_list')
 
 class DraftListView(LoginRequiredMixin, ListView):
     login_url = '/login/'
