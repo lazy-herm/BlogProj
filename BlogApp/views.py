e53c4bc0583f12d913109186c5d30a84f22278ac
Marc Vautheny <marc@vautheny.com>
2022-05-05 13:02:26 +0100

    Added post publish view function and url path

--- BlogApp/views.py
+++ BlogApp/views.py
@@ -56,6 +56,14 @@ class DraftListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
 
+
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail',pk=pk)
+
+
 @login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
