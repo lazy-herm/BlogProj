a43e3c34475d855c5cae22c528b8411b00a6f2e9
Marc Vautheny <marc@vautheny.com>
2022-05-05 09:42:45 +0100

    Added comment remove view function and url path

--- BlogApp/views.py
+++ BlogApp/views.py
@@ -75,3 +75,11 @@ def comment_approve(request,pk):
     comment = get_object_or_404(Comments, pk=pk)
     comment.approve()
     return  redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comments, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
