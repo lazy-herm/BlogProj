327831c90dd0dbcfaf924f9e24ee5d7cb2f5a5e2
Marc Vautheny <marc@vautheny.com>
2022-05-05 08:10:51 +0100

    Fixing runserver errors
    - corrected url pattern for about view
    - endblock typo correction
    - runserver runs with no errors

--- BlogApp/views.py
+++ BlogApp/views.py
@@ -1,9 +1,10 @@
 from os import lseek
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
 from BlogApp.models import Post, Comments
 from BlogApp.forms import PostForm, CommentForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.urls import reverse_lazy
 # Create your views here.
@@ -11,7 +12,7 @@ from django.urls import reverse_lazy
 
 class AboutView(TemplateView):
     template_name = 'blog/about.html'
-    ##test for git alias
+
 class PostListView(ListView):
     model = Post
 
@@ -44,7 +45,7 @@ class DraftPostListView(ListView):
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
 
-    success_url = reverse_lazy('blog/post_list')
+    success_url = reverse_lazy('post_list')
 
 class DraftListView(LoginRequiredMixin, ListView):
     login_url = '/login/'
@@ -53,3 +54,7 @@ class DraftListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
--- BlogProj/settings.py
+++ BlogProj/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR =os.path.join('BASE_DIR', 'BlogApp/templates/blog')
+TEMPLATE_DIR =os.path.join('BASE_DIR', 'BlogApp/templates/blog/')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
