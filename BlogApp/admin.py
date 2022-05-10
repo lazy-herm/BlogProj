7ffd01ba9c742439abc932225a29d4c4f6092212
Marc Vautheny <marc@vautheny.com>
2022-05-10 21:48:28 +0100

    Update login and logout view import due to use of later django version compared to course examples

--- BlogApp/admin.py
+++ BlogApp/admin.py
@@ -1,3 +1,7 @@
+from importlib import import_module
 from django.contrib import admin
-
+from BlogApp.models import Post, Comments
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Comments)
