4e249905129d4e2cf178dab6f6ae57c0876c29b9
Marc Vautheny <marc@vautheny.com>
2022-06-07 07:51:36 +0100

    Font fix Login form tag typo Changed views template references due to changed folder name More url commenting typo fixes Post detail now showing text instead of just title Approve_comment to approved_comment typo Post date now showing in post list

--- BlogProj/urls.py
+++ BlogProj/urls.py
@@ -20,6 +20,6 @@ from django.contrib.auth import views
 urlpatterns = [
     path('', include('BlogApp.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/login', views.LoginView.as_view(), name='login'),
-    path('accounts/logout',views.LogoutView.as_view(), name='logout', kwargs={'next_page':'/'})
+    path('accounts/', include('django.contrib.auth.urls')),
+    
 ]
